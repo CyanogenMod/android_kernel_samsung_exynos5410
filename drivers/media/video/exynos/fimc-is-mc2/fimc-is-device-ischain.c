ad4c069b7ac760fb2e3addaec59de459ba2b4f44
Ethan Chen <intervigil@gmail.com>
2014-07-23 15:10:48 -0700

    [media] fimc: Move camera file paths to sane location
    Change-Id: I16991596550e24aacbbf4c7c184e67551b7acd4c

--- drivers/media/video/exynos/fimc-is-mc2/fimc-is-device-ischain.c
+++ drivers/media/video/exynos/fimc-is-mc2/fimc-is-device-ischain.c
@@ -59,7 +59,7 @@
 #include "fimc-is-device-ischain.h"
 
 #define SDCARD_FW
-#define FIMC_IS_SETFILE_SDCARD_PATH		"/data/media/0/"
+#define FIMC_IS_SETFILE_SDCARD_PATH		"/data/camera/"
 #define FIMC_IS_SDCARD_FW			"fimc_is_fw2.bin"
 #define FIMC_IS_GUMI_FW				"fimc_is_gumi_fw2.bin"
 #define FIMC_IS_SEC_FW				"fimc_is_sec_fw2.bin"
@@ -72,9 +72,9 @@
 #define FIMC_IS_6B2_SETF			"setfile_6b2.bin"
 #define FIMC_IS_4E5_SETF			"setfile_4e5.bin"
 
-#define FIMC_IS_FW_SDCARD			"/data/media/0/fimc_is_fw2.bin"
+#define FIMC_IS_FW_SDCARD			"/data/camera/fimc_is_fw2.bin"
 #define FIMC_IS_FW_PATH				"/system/vendor/firmware/"
-#define FIMC_IS_FW_DUMP_PATH			"/data/"
+#define FIMC_IS_FW_DUMP_PATH			"/data/camera/"
 #define FIMC_IS_FW_BASE_MASK			((1 << 26) - 1)
 #define FIMC_IS_VERSION_SIZE			43
 #define FIMC_IS_SETFILE_VER_OFFSET		0x40
@@ -83,7 +83,7 @@
 
 #define BINNING(x, y) ((1 << (((x) / (y)) >> 1)) * 1000)
 
-#define FIMC_IS_CAL_SDCARD			"/data/cal_data.bin"
+#define FIMC_IS_CAL_SDCARD			"/data/camera/cal_data.bin"
 /*#define FIMC_IS_MAX_CAL_SIZE			(20 * 1024)*/
 #define FIMC_IS_MAX_FW_SIZE			(2048 * 1024)
 #define FIMC_IS_CAL_START_ADDR			(0x013D0000)
@@ -1612,7 +1612,7 @@ void fimc_is_ischain_savefirm(struct fimc_is_device_ischain *this)
 #ifdef DEBUG_DUMP_FIRMWARE
 	loff_t pos;
 
-	write_data_to_file("/data/firmware.bin", (char *)this->imemory.kvaddr,
+	write_data_to_file("/data/camera/firmware.bin", (char *)this->imemory.kvaddr,
 		(size_t)FIMC_IS_A5_MEM_SIZE, &pos);
 #endif
 }
